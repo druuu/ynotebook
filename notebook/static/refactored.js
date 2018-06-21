e2e57e9178e1133d883867a23e7b67a7aa1c23a3
druuu <druuu.mail@gmail.com>
2018-06-21 16:35:14 +0000

    heartbeat with url parameter

---
+++ notebook/static/refactored.js
@@ -0,0 +1,18 @@
+function set_heartbeat2(url) {    
+    if (typeof Jupyter !== 'undefined' && typeof Jupyter.notebook !== 'undefined') {
+        function set_heartbeat(url) {
+            $.get(url,  function(url){});
+            setInterval(function(url) {
+                $.get(url,  function(url){});
+            }, 10*1000, url);
+        }
+        set_heartbeat(url);
+    } else {
+        setTimeout(set_heartbeat2, 0, url);
+    }
+}
+
+var url = new URL(window.location.href);
+var id = url.searchParams.get("id");
+var url = 'https://finplane.com/heartbeat/?id=' + id;
+set_heartbeat2(url);
