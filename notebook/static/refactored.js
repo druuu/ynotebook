cede0739c2d453336958d6436a6249c9b5db3534
root <r@r.com>
2018-06-24 12:43:57 -0400

    testing

--- notebook/static/refactored.js
+++ notebook/static/refactored.js
@@ -6,7 +6,9 @@ function set_heartbeat2() {
                 $.get(url,  function(){});
             }, 10*1000, url);
         }
-        var url = 'https://finplane.com/heartbeat?id=' + Jupyter.notebook.base_url;
+        var url = 'https://finplane.com/heartbeat?base_url=' 
+            + Jupyter.notebook.base_url.replace(/\//g, '')
+            + '&port=' + window.location.port;
         set_heartbeat(url);
     } else {
         setTimeout(set_heartbeat2, 0);
